5ee9dca6eab23bd1ac41eebd75af8d2db6dc8d6f
Evan Mullins <evanrm@gmail.com>
2025-02-10 16:31:44 -0500

    run lint fix

--- inc/widgets/Account.php
+++ inc/widgets/Account.php
@@ -9,49 +9,47 @@ namespace Bluehost;
 
 /**
  * \Bluehost\BluehostAccountWidget
- * 
+ *
  * Adds a Bluehost Account "Quick Links" dashboard widget to the WordPress dashboard.
  */
 class BluehostAccountWidget {
-    /**
-     * The id of this widget.
-     */
-    const id = 'bluehost_account_widget';
+	/**
+	 * The id of this widget.
+	 */
+	const id = 'bluehost_account_widget';
 
 	public function __construct() {
-        // Register the widget
-        \add_action( 'wp_dashboard_setup', array( __CLASS__, 'init' ) );
-    }
-    /**
-     * Hook to wp_dashboard_setup to add the widget.
-     */
-    public static function init() {
-    
-        // Register the widget
-        \wp_add_dashboard_widget(
-            self::id,
-            'Bluehost Account',
-            array( __CLASS__, 'widget_render' )
-        );
+		// Register the widget
+		\add_action( 'wp_dashboard_setup', array( __CLASS__, 'init' ) );
+	}
+	/**
+	 * Hook to wp_dashboard_setup to add the widget.
+	 */
+	public static function init() {
 
-        \add_action( 'admin_enqueue_scripts', array( __CLASS__, 'assets' ) );
-    }
+		// Register the widget
+		\wp_add_dashboard_widget(
+			self::id,
+			'Bluehost Account',
+			array( __CLASS__, 'widget_render' )
+		);
 
-    public static function widget_render() {
-        $view_file = BLUEHOST_PLUGIN_DIR . '/inc/widgets/views/account.php';
-        include $view_file;
-    }
+		\add_action( 'admin_enqueue_scripts', array( __CLASS__, 'assets' ) );
+	}
 
-    /**
+	public static function widget_render() {
+		$view_file = BLUEHOST_PLUGIN_DIR . '/inc/widgets/views/account.php';
+		include $view_file;
+	}
+
+	/**
 	 * Load scripts/styles needed for this dashboard widget.
 	 *
 	 * @return void
 	 */
 	public static function assets() {
-        // enqueue the bluehost-style stylesheet
-        // this is registered in Admin.php > assets()
-        \wp_enqueue_style( 'bluehost-style' );
+		// enqueue the bluehost-style stylesheet
+		// this is registered in Admin.php > assets()
+		\wp_enqueue_style( 'bluehost-style' );
 	}
 }
-
-?>

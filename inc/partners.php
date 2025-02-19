a549d156303aa4005fbf641f20ec5861c4ff656a
Evan Mullins <evanrm@gmail.com>
2025-02-19 13:58:09 -0500

    temporarily remove wpforms and aioseo filters with comment

--- inc/partners.php
+++ inc/partners.php
@@ -17,8 +17,8 @@ namespace Bluehost;
 function wpforms_upgrade_affiliate_link( $url ) {
 	return 'http://www.shareasale.com/r.cfm?B=837827&U=1258907&M=64312&urllink=' . rawurlencode( $url );
 }
-
-add_filter( 'wpforms_upgrade_link', __NAMESPACE__ . '\\wpforms_upgrade_affiliate_link' );
+// Remove filter temporarily while SaS issue is resolved
+// add_filter( 'wpforms_upgrade_link', __NAMESPACE__ . '\\wpforms_upgrade_affiliate_link' );
 
 /**
  * All-in-One SEO upgrade affiliate link.
@@ -30,8 +30,8 @@ add_filter( 'wpforms_upgrade_link', __NAMESPACE__ . '\\wpforms_upgrade_affiliate
 function aioseo_upgrade_affiliate_link( $url ) {
 	return 'http://www.shareasale.com/r.cfm?B=1491200&U=1258907&M=94778&urllink=' . rawurlencode( $url );
 }
-
-add_filter( 'aioseo_upgrade_link', __NAMESPACE__ . '\\aioseo_upgrade_affiliate_link' );
+// Remove filter temporarily while SaS issue is resolved
+// add_filter( 'aioseo_upgrade_link', __NAMESPACE__ . '\\aioseo_upgrade_affiliate_link' );
 
 /**
  * Enable Sharing with WooCommerce

a21c14eb9b9aea5dc249098ed9fd41949f0a8d2e
Evan Mullins <evanrm@gmail.com>
2025-02-19 11:55:42 -0500

    update `set-latest-wp-version` script to also update the tested up to version in main plugin file

--- scripts/set-latest-wp-version.js
+++ scripts/set-latest-wp-version.js
@@ -1,9 +1,14 @@
 /**
  * This sets the latest wp-version in the wp-env.json file.
- * It keeps us updated every time we run `npm i` so we don't have to remember.
+ * It runs every `npm i` so we don't have to manuallly update to latest versions.
  */
-
-const { writeFile } = require( 'fs/promises' );
+const fs = require( 'fs' );
+const path = require( 'path' );
+const wpEnvFile = path.resolve( __dirname, '../.wp-env.json' );
+const pluginFile = path.resolve(
+	__dirname,
+	'../bluehost-wordpress-plugin.php'
+);
 const wpEnv = require( '../.wp-env.json' );
 async function fetchData( url ) {
 	try {
@@ -25,23 +30,42 @@ async function fetchData( url ) {
 
 fetchData( 'https://api.wordpress.org/core/stable-check/1.0/' ).then(
 	( json ) => {
+		const wpEnvData = require( wpEnvFile );
+		const wpCurrent = wpEnvData.core.split( '#tags/' )[ 1 ];
 		const wpVersion = Object.keys( json )[ Object.keys( json ).length - 1 ];
-		wpEnv.core = `WordPress/WordPress#tags/${ wpVersion }`;
-		writeFile(
-			'../.wp-env.json',
-			JSON.stringify( wpEnv, null, 2 ),
-			'utf8',
-			( err ) => {
+		if ( wpCurrent === wpVersion ) {
+			console.log( 'No WordPress updates found.' );
+			return;
+		}
+		// update data
+		wpEnvData.core = `WordPress/WordPress#tags/${ wpVersion }`;
+		// write update to wp-env file
+		try {
+			fs.writeFileSync( wpEnvFile, JSON.stringify( wpEnvData, null, 2 ) );
+			console.log(
+				`The .wp-env.json file was updated with the latest WordPress version (${ wpVersion }).`
+			);
+		} catch ( err ) {
+			console.log(
+				'An error occurred while writing latest WordPress version to .wp-env.json file.',
+				err
+			);
+		}
+
+		// write to plugin file
+		fs.readFile( pluginFile, 'utf8', function ( err, data ) {
+			if ( err ) {
+				return console.log( err );
+			}
+			const result = data.replaceAll( wpCurrent, wpVersion );
+			fs.writeFile( pluginFile, result, 'utf8', function ( err ) {
 				if ( err ) {
-					console.log(
-						'An error occurred while writing latest WordPress version to .wp-env.json file.'
-					);
 					return console.log( err );
 				}
-				console.log(
-					`The .wp-env.json file was updated with the latest WordPress version (${ wpVersion }).`
-				);
-			}
-		);
+			} );
+			console.log(
+				`The plugin "Tested up to:" value has been updated to ${ wpVersion }.`
+			);
+		} );
 	}
 );

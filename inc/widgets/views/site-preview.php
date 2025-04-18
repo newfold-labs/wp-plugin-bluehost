2b9365d6a197f96bddd6990e54db0e51f326255c
Evan Mullins <evanrm@gmail.com>
2025-04-18 11:56:36 -0400

    fix color contrast on site status text for a11y

--- inc/widgets/views/site-preview.php
+++ inc/widgets/views/site-preview.php
@@ -96,7 +96,7 @@ $site_edit_url = get_admin_url( null, wp_is_block_theme() ? 'site-editor.php?can
 		width: 1.5rem;
 	}
 	.iframe-preview-status.status-coming-soon {
-		color: rgb(225 0 1);
+		color: rgb(205 0 1);
 	}
 	.iframe-preview-status.status-live {
 		color: rgb(0 109 18);

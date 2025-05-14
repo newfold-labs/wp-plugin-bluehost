6c58827dc608679874079724188bd3d781b456bd
Evan Mullins <evanrm@gmail.com>
2025-05-14 18:26:12 -0400

    lint fixes

--- inc/GoogleSiteKit.php
+++ inc/GoogleSiteKit.php
@@ -19,20 +19,20 @@ class GoogleSiteKit {
 	/**
 	 * Check if Google Site Kit is enabled and set the transient value.
 	 *
-	 * @param mixed $value The transient value.
-	 * @param int   $expiration The expiration time.
+	 * @param mixed  $value The transient value.
+	 * @param int    $expiration The expiration time.
 	 * @param string $transient The transient name.
 	 *
 	 * @return mixed
 	 */
 	public function maybe_enable_google_site_kit( $value, $expiration, $transient ) {
-		if( class_exists( 'WPSEO_Options' ) ) {
-			$option_value = \WPSEO_Options::get( 'google_site_kit_feature_enabled', null, [ 'wpseo' ] );
+		if ( class_exists( 'WPSEO_Options' ) ) {
+			$option_value = \WPSEO_Options::get( 'google_site_kit_feature_enabled', null, array( 'wpseo' ) );
 			if (
 				! $option_value &&
-				is_array($value) &&
+				is_array( $value ) &&
 				array_key_exists( 'google_site_kit_feature_enabled', $value ) &&
-				true === $value[ 'google_site_kit_feature_enabled' ]
+				true === $value['google_site_kit_feature_enabled']
 			) {
 				\WPSEO_Options::set( 'google_site_kit_feature_enabled', true, 'wpseo' );
 			}

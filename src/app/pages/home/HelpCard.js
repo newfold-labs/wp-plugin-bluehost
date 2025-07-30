c1b1976d509caa57efbbd13c04b3ccf66884a776
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-07-30 10:26:50 +0100

    Fix Get Help & Resources link

--- src/app/pages/home/HelpCard.js
+++ src/app/pages/home/HelpCard.js
@@ -13,7 +13,7 @@ const HelpCard = ( {} ) => {
 	};
 	return (
 		<a
-			href={ window.NewfoldRuntime.linkTracker.addUtmParams( '#/help' ) }
+			href={ window.NewfoldRuntime?.linkTracker?.addUtmParams( 'admin.php?page=bluehost#/help' ) }
 			onClick={ toggleHelp }
 			className="nfd-no-underline nfd-card-link"
 			data-cy="help-card"

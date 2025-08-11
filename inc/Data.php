40b9bcfd8e3aed58158909125012057ae228332d
Evan Mullins <evanrm@gmail.com>
2025-08-11 13:24:46 -0400

    linting

--- inc/Data.php
+++ inc/Data.php
@@ -33,10 +33,10 @@ final class Data {
 		);
 
 		if ( class_exists( 'NewfoldLabs\WP\Module\Solutions\Solutions' ) ) {
-			$solution_data = Solutions::get_enhanced_entitlment_data();
-			$solution = is_array( $solution_data ) && array_key_exists('solution', $solution_data) ? $solution_data['solution'] : false;
+			$solution_data        = Solutions::get_enhanced_entitlment_data();
+			$solution             = is_array( $solution_data ) && array_key_exists( 'solution', $solution_data ) ? $solution_data['solution'] : false;
 			$runtime['solutions'] = array(
-				'solution' => $solution,
+				'solution'   => $solution,
 				'wondercart' => self::get_entitlement_by_id( $solution_data, 'WonderCart' ),
 			);
 		}
@@ -44,7 +44,7 @@ final class Data {
 		// Add solution ecom family ctb to runtime
 		$runtime['ctbs'] = array(
 			'ecomFamily' => array(
-				'id' => '5dc83bdd-9274-4557-a6d7-0b2adbc3919f',
+				'id'  => '5dc83bdd-9274-4557-a6d7-0b2adbc3919f',
 				'url' => 'https://www.bluehost.com/my-account/hosting/details#click-to-buy-WP_SOLUTION_FAMILY',
 			),
 		);

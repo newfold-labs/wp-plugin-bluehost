3ed827680faa5897cfdfc9da991d704c334e06ad
Evan Mullins <evanrm@gmail.com>
2025-09-29 17:44:35 -0400

    lint cleanup

--- src/app/pages/commerce/index.js
+++ src/app/pages/commerce/index.js
@@ -5,9 +5,21 @@ import { __ } from '@wordpress/i18n';
 const CommercePage = () => {
 	return (
 		<Page className={ 'wppbh-app-commerce-page nfd-w-full' }>
-			<div className={ 'wppbh-app-help-page__header nfd-flex nfd-flex-col nfd-gap-y-4' }>
-				<Title as="h1">{ __( 'Premium tools available in eCommerce Add-Ons', 'wp-plugin-bluehost' ) }</Title>
-				<Title as="h2" className="nfd-font-normal nfd-text-[13px] nfd-color-body">
+			<div
+				className={
+					'wppbh-app-help-page__header nfd-flex nfd-flex-col nfd-gap-y-4'
+				}
+			>
+				<Title as="h1">
+					{ __(
+						'Premium tools available in eCommerce Add-Ons',
+						'wp-plugin-bluehost'
+					) }
+				</Title>
+				<Title
+					as="h2"
+					className="nfd-font-normal nfd-text-[13px] nfd-color-body"
+				>
 					{ __(
 						'Discover exclusive features, designed to deliver unmatched value and elevate your online experience.',
 						'wp-plugin-bluehost'

324aedbb841b0e3c94c9cb99a8502b04ac7ced76
wpalani <abdulrahmanalani95@gmail.com>
2025-10-03 11:46:52 -0700

    Bump onboarding-data

--- bluehost-wordpress-plugin.php
+++ bluehost-wordpress-plugin.php
@@ -15,7 +15,7 @@
  * Version:           4.6.2
  * Requires PHP:      7.3
  * Requires at least: 6.6
- * Tested up to:      6.8.2
+ * Tested up to:      6.8.3
  * Author:            Bluehost
  * Author URI:        https://bluehost.com
  * Text Domain:       wp-plugin-bluehost

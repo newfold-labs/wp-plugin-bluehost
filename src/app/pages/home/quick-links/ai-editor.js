ec7b59c5371d0aaccb51a0aa5131ccc5bbe7fbef
Evan Mullins <evanrm@gmail.com>
2025-12-10 14:11:31 -0500

    lint it

--- src/app/pages/home/quick-links/ai-editor.js
+++ src/app/pages/home/quick-links/ai-editor.js
@@ -23,35 +23,38 @@ export default () => {
 			data-test-id="quick-links-ai-editor-link"
 			href={ editorUrl }
 		>
-		<div
-			className={
-				'nfd-quick-links-card__head nfd-flex nfd-items-center nfd-gap-2'
-			}
-		>
-			<span
+			<div
 				className={
-					'nfd-quick-links-card__icon nfd-p-[4px] nfd-bg-[#ddecff] nfd-rounded-[5px]'
+					'nfd-quick-links-card__head nfd-flex nfd-items-center nfd-gap-2'
 				}
 			>
-				<AiEditorIcon />
-			</span>
-			<Title as={ 'h2' } className={ 'nf-text-base nfd-font-semibold' }>
-				{ __( 'Launch our AI Editor', 'wp-plugin-bluehost' ) }
-			</Title>
-		</div>
-		<p className="nfd-mt-0 nfd-text-body">
-			{ __(
-				'Say hello to effortless site building with a wide library of 250+ blocks',
-				'wp-plugin-bluehost'
-			) }
-		</p>
-		<p
-			className={
-				'nfd-mt-0 nfd-font-semibold nfd-no-underline nfd-text-[#196BDE]'
-			}
-		>
-			{ __( 'Launch the Editor', 'wp-plugin-bluehost' ) }
-		</p>
-	</Card>
+				<span
+					className={
+						'nfd-quick-links-card__icon nfd-p-[4px] nfd-bg-[#ddecff] nfd-rounded-[5px]'
+					}
+				>
+					<AiEditorIcon />
+				</span>
+				<Title
+					as={ 'h2' }
+					className={ 'nf-text-base nfd-font-semibold' }
+				>
+					{ __( 'Launch our AI Editor', 'wp-plugin-bluehost' ) }
+				</Title>
+			</div>
+			<p className="nfd-mt-0 nfd-text-body">
+				{ __(
+					'Say hello to effortless site building with a wide library of 250+ blocks',
+					'wp-plugin-bluehost'
+				) }
+			</p>
+			<p
+				className={
+					'nfd-mt-0 nfd-font-semibold nfd-no-underline nfd-text-[#196BDE]'
+				}
+			>
+				{ __( 'Launch the Editor', 'wp-plugin-bluehost' ) }
+			</p>
+		</Card>
 	);
 };

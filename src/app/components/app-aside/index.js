fff6445156ca5a3dd5b2b38b5fbc0dd15952af69
Evan Mullins <evanrm@gmail.com>
2026-02-02 18:29:04 -0500

    minor cleanup

--- src/app/components/app-aside/index.js
+++ src/app/components/app-aside/index.js
@@ -5,6 +5,10 @@ import { ReactComponent as ArrowRightIcon } from '../../images/yoast-ad-arrow.sv
 import { ReactComponent as BulletPointIcon } from '../../images/yoast-ad-check.svg';
 import './yoast-ad.scss';
 
+/**
+ * App Aside
+ * @return {JSX.Element} app aside component
+ */
 export const AppAside = () => {
 	return (
 		<div className="wppbh-app-aside">
@@ -16,8 +20,8 @@ export const AppAside = () => {
 };
 
 /**
- * Yoast Ad 
- * @returns {JSX.Element} yoast ad component
+ * Yoast Ad
+ * @return {JSX.Element} yoast ad component
  */
 const YoastAd = () => {
 	return (
@@ -31,7 +35,9 @@ const YoastAd = () => {
 					height="64"
 				/>
 			</figure>
-			<h2 className="yoast-ad-title nfd-title nfd-title--2 nfd-mt-6 nfd-text-xl nfd-font-semibold">Yoast SEO Premium</h2>
+			<h2 className="yoast-ad-title nfd-title nfd-title--2 nfd-mt-6 nfd-text-xl nfd-font-semibold">
+				Yoast SEO Premium
+			</h2>
 			<p className="nfd-mt-3 nfd-font-medium nfd-text-slate-800">
 				Spend less time on SEO tasks!
 			</p>
@@ -41,7 +47,8 @@ const YoastAd = () => {
 			<ul className="nfd-list-outside nfd-text-slate-600 nfd-mt-4 nfd-flex nfd-flex-col nfd-gap-2">
 				<li className="nfd-flex nfd-items-start">
 					<BulletPointIcon />
-					Create optimized SEO titles &amp; meta descriptions in seconds
+					Create optimized SEO titles &amp; meta descriptions in
+					seconds
 				</li>
 				<li className="nfd-flex nfd-items-start">
 					<BulletPointIcon />
@@ -58,15 +65,18 @@ const YoastAd = () => {
 			</ul>
 			<a
 				className="nfd-button nfd-button--upsell nfd-flex nfd-justify-center nfd-gap-2 nfd-mt-4 focus:nfd-ring-offset-primary-500"
-				href="https://yoa.st/jj?php_version=8.3&platform=wordpress&platform_version=6.9&software=free&software_version=26.8&days_active=3&user_language=en_US&screen=wpseo_dashboard" 
+				href="https://yoa.st/jj?php_version=8.3&platform=wordpress&platform_version=6.9&software=free&software_version=26.8&days_active=3&user_language=en_US&screen=wpseo_dashboard"
 				target="_blank"
-				rel="noopener"
-				data-action="load-nfd-ctb" data-ctb-id="f6a84663-465f-4cb5-8ba5-f7a6d72224b2"
+				rel="noopener noreferrer"
+				data-action="load-nfd-ctb"
+				data-ctb-id="f6a84663-465f-4cb5-8ba5-f7a6d72224b2"
 			>
 				<span>Buy now</span>
 				<ArrowRightIcon />
 			</a>
-			<p className="nfd-text-center nfd-text-xs nfd-font-normal nfd-leading-5 nfd-text-slate-500 nfd-italic nfd-mt-3 nfd-mb-2">Less friction. Faster publishing.</p>
+			<p className="nfd-text-center nfd-text-xs nfd-font-normal nfd-leading-5 nfd-text-slate-500 nfd-italic nfd-mt-3 nfd-mb-2">
+				Less friction. Faster publishing.
+			</p>
 			<hr className="nfd-border-t nfd-border-slate-200 nfd-my-4" />
 			<ul className="nfd-text-center nfd-text-xs nfd-font-medium nfd-text-slate-800 nfd-list-none">
 				<li>30-day money back guarantee</li>

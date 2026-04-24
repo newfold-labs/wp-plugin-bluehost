cbb72cbbd9447cc09986866fd7558b712695d3bb
Vikash Rana <vikasrana1998@gmail.com>
2026-04-25 00:32:22 +0530

    Update "Edit Site" button on plugin dashboard to point to 10web Editor when on AI Site

--- inc/widgets/views/site-preview.php
+++ inc/widgets/views/site-preview.php
@@ -45,8 +45,10 @@ $svgPencil = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 2
 $svgExternalView = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
 </svg>';
-// site editor if block theme, otherwise customizer
-$site_edit_url = buildLink( get_admin_url( null, wp_is_block_theme() ? 'site-editor.php?canvas=edit' : 'customize.php' ), array( 'source' => 'widget=bluehost_account_widget' ) );
+$site_edit_url = buildLink(
+	Filters::get_site_edit_admin_url(),
+	array( 'source' => 'widget=bluehost_account_widget' )
+);
 ?>
 <style>
 	#site_preview_widget h2 {

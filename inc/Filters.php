24f59db5e351fc190eb10ed224896ee58cd3aa73
Vikash Rana <vikasrana1998@gmail.com>
2026-05-08 17:19:53 +0530

    test

--- inc/Filters.php
+++ inc/Filters.php
@@ -40,6 +40,7 @@ final class Filters {
 			$args['headers']['X-WP-LOCALE']      = get_locale();
 			$args['headers']['X-HOST-PLUGIN-ID'] = $container->plugin()->id;
 		}
+		error_log( "args: " . print_r( $args, true ) );
 
 		return $args;
 	}
